78a21a6a3f27155d919ac5d207043cb446861959
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2025-04-18 10:43:44 -0600

    navbar

---
+++ src/routes/paid/+page.svelte
@@ -0,0 +1,28 @@
+<div class="rounded-box border-base-content/5 bg-base-100 overflow-x-auto border">
+	<table class="table">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th>Departamento</th>
+				<th>Ultimo pago</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- row 1 -->
+			<tr>
+				<td>A3</td>
+				<td>Enero 2025</td>
+			</tr>
+			<!-- row 2 -->
+			<tr>
+				<td>M1</td>
+				<td>Febrero 2025</td>
+			</tr>
+			<!-- row 3 -->
+			<tr>
+				<td>N4</td>
+				<td>Marzo 2025</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
