96084f04bc1d2c657713a591bd7e558ab876149f
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2025-04-28 09:44:42 -0600

    DB create

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,14 +2,62 @@ import { fail, redirect } from '@sveltejs/kit';
 import { invalidateSession, deleteSessionTokenCookie } from '$lib/server/session';
 import client from '$lib/server/db.js';
 
+export const load = async () => {
+	try {
+		const mongoClient = await client.connect();
+		const db = mongoClient.db('adminhood');
+		const newsCollection = db.collection('news');
+		const options = {
+			sort: { createdAt: -1 },
+			projection: { _id: 0 }
+		};
+
+		const rawNews = await newsCollection.find({}, options).toArray();
+
+		const news = rawNews.map((order) => ({
+			...order,
+			createdAt: order.createdAt.toLocaleTimeString('en-US', {
+				month: '2-digit',
+				day: '2-digit',
+				hour: '2-digit',
+				minute: '2-digit',
+				hour12: true,
+				timeZone: 'America/Mexico_City'
+			})
+		}));
+
+		return {
+			news
+		};
+	} catch (e) {
+		console.log(e);
+	}
+};
+
 export const actions = {
+	post: async ({ request }) => {
+		const data = await request.formData();
+		const post = data.get('new');
+		try {
+			const mongoClient = await client.connect();
+			const db = mongoClient.db('adminhood');
+			const news = db.collection('news');
+			const doc = {
+				post,
+				createdAt: new Date()
+			};
+			await news.insertOne(doc);
+		} catch (error) {
+			console.log(error);
+		}
+	},
 	adminMode: async ({ locals }) => {
 		const isAdmin = locals.user.admin;
 		const googleId = locals.user.googleId;
 
 		try {
 			const mongoClient = await client.connect();
-			const db = mongoClient.db('chucky');
+			const db = mongoClient.db('adminhood');
 			const orders = db.collection('users');
 			const filter = { googleId };
 			const updateDoc = {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,22 @@
+<script>
+</script>
+
 <div class="card card-border bg-base-100 w-96">
 	<div class="card-body">
-		<h2 class="card-title">Card Title</h2>
+		<h2 class="card-t itle">Card Title</h2>
 		<p>
 			A card component has a figure, a body part, and inside body there are title and actions parts
 		</p>
-		<fieldset class="fieldset">
-			<legend class="fieldset-legend">Your bio</legend>
-			<textarea class="textarea h-24" placeholder="Bio"></textarea>
-			<div class="label">Optional</div>
-		</fieldset>
+		<form method="POST" action="?/post">
+			<fieldset class="fieldset">
+				<legend class="fieldset-legend">Your bio</legend>
+				<textarea class="textarea h-24" placeholder="Bio" name="new"></textarea>
+				<div class="label">Optional</div>
+			</fieldset>
 
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Buy Now</button>
-		</div>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Buy Now</button>
+			</div>
+		</form>
 	</div>
 </div>
